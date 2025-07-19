a992c8439dc5991d3c0b219b7070090c7942ca51
rolex <wergerdifeok.sv.r.r.1058@gmail.com>
2025-07-19 11:36:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'https://42b129950777ab.lhr.life',
+        target: process.env.VITE_API_URL || 'http://13.53.133.100:3001',
         changeOrigin: true,
         secure: false
       }
