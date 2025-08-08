63bbdfaa04e3a07d3aea656fbca8b31330cb018f
rolex <wergerdifeok.sv.r.r.1058@gmail.com>
2025-08-08 22:51:12 +0530

    Update instagramService.js

--- src/services/instagramService.js
+++ src/services/instagramService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = (import.meta.env.VITE_API_URL || 'https://plot-geek-vehicle-pathology.trycloudflare.com') + '/api';
+const API_BASE_URL = (import.meta.env.VITE_API_URL || 'https://probability-if-destiny-strategic.trycloudflare.com') + '/api';
 
 // List of session cookies (sessionid and csrftoken pairs)
 const accounts = [
